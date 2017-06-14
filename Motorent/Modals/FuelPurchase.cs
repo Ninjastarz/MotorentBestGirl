8cadea0adabd8ead1fd0bbbeea12cee99e560ea0
Levi <Levi44@hotmail.co.uk>
2017-06-14 16:47:54 +0800

    Add files via upload

---
+++ Motorent/Modals/FuelPurchase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modals
+{
+    public class FuelPurchase
+    {
+        public int PurchasedAmount;
+        public double FuelCost;
+        public FuelPurchase(int purchasedAmount, double fuelCost)
+        {
+            PurchasedAmount = purchasedAmount;
+            FuelCost = fuelCost;
+        }
+    }
+}
